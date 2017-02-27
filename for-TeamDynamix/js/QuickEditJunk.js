2f4bdb757a72afc94aeaf07dff6924519f2e7420
Unknown <phippsg@oregonstate.edu>
2017-02-27 10:07:45 -0800

    TD Sucks

--- for-TeamDynamix/js/QuickEditJunk.js
+++ for-TeamDynamix/js/QuickEditJunk.js
@@ -2,7 +2,7 @@
 // @name         Mark Junk
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  TD is a potato and should feel bad.
 // @author       You
 // @match        https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/TicketEdit*
 // @grant        none
@@ -11,18 +11,39 @@
 (function() {
     'use strict';
 
-    var detailSaveBar = document.getElementsByClassName("TabButtonCell")[0];
+    var detailSaveBar = (document.getElementsByClassName("TabButtonCell"))[0];
     var btnMarkJunk = document.createElement("button");
     btnMarkJunk.className = "btn btn-danger";
     btnMarkJunk.innerHTML = "Mark Junk";
+    btnMarkJunk.type = "button";
     $( btnMarkJunk ).css("float", "right");
+    btnMarkJunk.setAttribute("id", "MarkJunk");
+
+    $( btnMarkJunk ).on("click", function() {
+        console.log($("#hdnTypeCategoryName").val());
+        console.log($("#hdnTypeName").val());
+        var tickTypeJSON = { "categoryname": "General", "typename": "Junk / Delete This Ticket" };
+        setTicketType(btnMarkJunk, tickTypeJSON);
+        console.log($("#hdnTypeCategoryName").val());
+        console.log($("#hdnTypeName").val());
+
+        //because TD sucks and we can't have nice things...
+        //update the text
+
+        //do Save action
+        WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions("btnSave", "", true, ";", "", false, false));
+        return false;
+    });
+
     detailSaveBar.appendChild(btnMarkJunk);
 
     //ticket type css selector: #upTicketType > div > div.form-control.talu_holder > span.bit-box
     //set to: General / Junk / Delete this ticket
 
-    var tickTypeHolder = document.getElementById("upTicketType");
-    console.log(tickTypeHolder.innerHTML);
-    var closer = tickTypeHolder[1];
-    var there = closer[1];
+    //var tickTypeHolder = document.getElementById("upTicketType");
+    //console.log(tickTypeHolder.innerHTML);
+    //var closer = tickTypeHolder[1];
+    //var there = closer[1];
+
+
 })();
