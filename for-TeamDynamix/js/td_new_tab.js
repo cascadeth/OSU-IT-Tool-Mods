3c8f77c83d5d350475dc465f9e01afcb3028cbae
cascadeth <cascadeth@gmail.com>
2017-03-02 14:38:51 -0800

    added new tab user script for Tampermonkey

---
+++ for-TeamDynamix/js/td_new_tab.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         TeamDynamix New Tab Override
+// @namespace    https://oregonstate.teamdynamix.com
+// @version      0.1
+// @description  Override default TD "new tab" settings
+// @author       You
+// @match        http://tampermonkey.net/index.php?version=4.2&ext=dhdg&updated=true
+// @grant        none
+// @include      https://oregonstate.teamdynamix.com/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.setCookie({name: 'OpenAllNewWin', value: '1'});
+})();
