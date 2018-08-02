f9b46678de32df240274a9da84f2a9e2f20a8b5a
farnhamt <farnhamt@oregonstate.edu>
2018-08-02 14:00:27 -0700

    Update Move Status to Top.user.js

--- TeamDynamix/Scripts/Move Status to Top.user.js
+++ TeamDynamix/Scripts/Move Status to Top.user.js
@@ -2,7 +2,7 @@
 // @name         Move Status to Top
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Auto-uncheck Notify Responsible
+// @description  Moves the "Status" field of the ticket to the very top of the page when creating a new ticket and when editing a ticket.
 // @author       Tyler Farnham
 // @match        https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/New*
 // @match        https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/Edit*
