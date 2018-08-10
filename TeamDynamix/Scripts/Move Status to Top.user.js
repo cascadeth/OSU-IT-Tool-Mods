34a28462e42b2b12770662c414ed003123be78fa
farnhamt <farnhamt@oregonstate.edu>
2018-08-10 09:46:13 -0700

    Update Move Status to Top.user.js

--- TeamDynamix/Scripts/Move Status to Top.user.js
+++ TeamDynamix/Scripts/Move Status to Top.user.js
@@ -1,16 +1,33 @@
 // ==UserScript==
 // @name         Move Status to Top
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Moves the "Status" field of the ticket to the very top of the page when creating a new ticket and when editing a ticket.
+// @version      1.1
+// @description  Auto-uncheck Notify Responsible
 // @author       Tyler Farnham
 // @match        https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/New*
 // @match        https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/Edit*
 // @grant        none
 // ==/UserScript==
 
-var StatusInput = document.getElementById("attribute1306-grp");
+var URL = document.location.href;
+var StatusInput;
+var topElement;
+window.setTimeout(moveStatusFieldToTop, 1000);
+function moveStatusFieldToTop(){
+    if(URL.indexOf("New") > 0){
 
-var topElement = document.getElementById("divContent").childNodes[7].childNodes[6];
+        StatusInput = document.getElementById("attribute1306-grp");
 
-topElement.parentNode.insertBefore(StatusInput, topElement);
+        topElement = document.getElementById("divContent").childNodes[7].childNodes[6];
+
+        topElement.parentNode.insertBefore(StatusInput, topElement);
+    }
+
+    else{
+        StatusInput = document.getElementById("attribute1306-grp");
+
+        topElement = document.getElementById("divContent").childNodes[1].childNodes[2];
+
+        topElement.parentNode.insertBefore(StatusInput, topElement);
+    }
+}
