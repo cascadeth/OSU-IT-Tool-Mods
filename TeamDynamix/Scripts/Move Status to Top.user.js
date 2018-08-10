952cfb99caa10e8bc5b839c42a67f89e4ab99379
farnhamt <farnhamt@oregonstate.edu>
2018-08-10 09:47:29 -0700

    Update Move Status to Top.user.js

--- TeamDynamix/Scripts/Move Status to Top.user.js
+++ TeamDynamix/Scripts/Move Status to Top.user.js
@@ -10,24 +10,20 @@
 // ==/UserScript==
 
 var URL = document.location.href;
-var StatusInput;
+var StatusInput = document.getElementById("attribute1306-grp");
 var topElement;
 window.setTimeout(moveStatusFieldToTop, 1000);
 function moveStatusFieldToTop(){
     if(URL.indexOf("New") > 0){
-
-        StatusInput = document.getElementById("attribute1306-grp");
-
+        
         topElement = document.getElementById("divContent").childNodes[7].childNodes[6];
 
-        topElement.parentNode.insertBefore(StatusInput, topElement);
     }
 
     else{
-        StatusInput = document.getElementById("attribute1306-grp");
-
         topElement = document.getElementById("divContent").childNodes[1].childNodes[2];
 
-        topElement.parentNode.insertBefore(StatusInput, topElement);
     }
+        topElement.parentNode.insertBefore(StatusInput, topElement);
+    
 }
