c85d6422c351f2c2d7c800a697f2c9d45a183c15
farnhamt <farnhamt@oregonstate.edu>
2018-12-28 12:39:35 -0800

    Update Phone@log Button on Reftool.user.js

--- Reftool2/Scripts/Phone@log Button on Reftool.user.js
+++ Reftool2/Scripts/Phone@log Button on Reftool.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Phone@log Button on Reftool
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.1
 // @description  Adds a button to create a new phone@log ticket with the appropriate fields filled out
-// @author       Luke Miletta
+// @author       Tyler Farnham / Luke Miletta
 // @match        https://tools.is.oregonstate.edu/reftool2/*
 // @match        https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/New?formId=17631&RequestorUID=de751dc3-eeb7-e611-80cd-000d3a13db68&/phonelog
 // @match        https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/New?formId=17631&RequestorUID=de751dc3-eeb7-e611-80cd-000d3a13db68&/transcripts
+// @match        https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/New?formId=17631&RequestorUID=*&/Duo/*
 // @grant        none
 // ==/UserScript==
 
@@ -20,6 +21,9 @@ else if(URL.indexOf("&/phonelog") >=0){
 else if(URL.indexOf("&/transcripts") >=0){
     window.setTimeout(fill_form_transcripts, 500);
 }
+else if(URL.indexOf("&/Duo") >=0){
+    window.setTimeout(fill_form_Duo, 500);
+}
 
 function fill_form_transcripts(){
     var status = document.getElementById("attribute1306");
@@ -32,6 +36,7 @@ function fill_form_transcripts(){
         }
     }
     var by = document.getElementById("attribute3955");
+    console.log(URL.split("/")[8]);
     by = by.children;
     by[0].setAttribute("value", "793");
     by[0].innerText = "Phone";
@@ -39,6 +44,32 @@ function fill_form_transcripts(){
     title.value = "Transcripts Request";
     var body = document.getElementById("attribute2937");
     body.innerText = "Someone called asking about transcripts. They are a previous student and forgot their ID number. We referred them to the Office of the Registrar.";
+}
+
+function fill_form_Duo(){
+    console.log("Executing the duo function!");
+    var status = document.getElementById("attribute1306");
+    var customerUserName = URL.split("/")[9].split("@")[0];
+    var customerUPN = URL.split("/")[9];
+    status = status.children;
+    for(var i=0; i<status.length;i++){
+        if(status[i].innerText = "Open" && status[i].getAttribute("value") == "17739"){
+            status[i].innerText = "Closed";
+            status[i].setAttribute("value", "17742");
+            break;
+        }
+    }
+    var by = document.getElementById("attribute3955");
+    console.log(by);
+    by = by.children;
+    by[0].setAttribute("value", "793");
+    by[0].innerText = "Phone";
+    var title = document.getElementById("attribute1303");
+    title.value = "Transcripts Request";
+    var body = document.getElementById("attribute2937");
+    body.innerText = customerUserName + " called in to get Duo reactivated on their phone.";
+    var techNotes = document.getElementById("attribute53162");
+    techNotes.innerText = "I had them confirm their phone number and then sent them a reactivation link. Duo was reactivated on their device successfully.\n\nAll is well.";
 }
 
 function fill_form_generic(){
@@ -79,7 +110,6 @@ function put_button(){
         window.open(url, '_blank');
     }
 
-
     // 1. Create the button
     var button2 = document.createElement("form-button");
     button2.setAttribute("type", "button");
@@ -96,6 +126,43 @@ function put_button(){
     // Function that handles click of form button
     function click_transcripts_button(){
         var url = "https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/New?formId=17631&RequestorUID=de751dc3-eeb7-e611-80cd-000d3a13db68&/transcripts";
+        window.open(url, '_blank');
+    }
+
+    // 1. Create the button
+    var button3 = document.createElement("form-button");
+    button3.setAttribute("type", "button");
+    button3.innerHTML = "Duo Reactivation";
+    button3.setAttribute("class", "btn btn-default");
+    button3.setAttribute("id", "duo-button");
+
+    // 2. Append somewhere
+    search.appendChild(button3);
+
+    // 3. Add event handler
+    button3.addEventListener("click", click_duo_button);
+
+    // Function that handles click of form button
+    function click_duo_button(){
+        var elementHelper = document.getElementById("customer-details");
+        console.log(elementHelper.childNodes[3].childNodes[4].childNodes[3].innerHTML.split(">")[1].split("<")[0]);
+        //console.log(elementHelper.childNodes[3].childNodes[5].childNodes[3].innerHTML.split(">")[1].split("<")[0]);
+        try{
+            var firstName = elementHelper.childNodes[3].childNodes[4].childNodes[3].innerHTML.split(">")[1].split("<")[0];
+        }
+        catch(err){
+            alert("Look up the customer in reftool first!");
+            return;
+        }
+        try{
+            var UID = document.getElementsByClassName("pull-right")[2].toString().split("U=")[1];
+        }
+        catch(err){
+            alert("User does not have a TDx profile!");
+            return;
+        }
+        console.log(UID);
+        var url = "https://oregonstate.teamdynamix.com/TDNext/Apps/425/Tickets/New?formId=17631&RequestorUID=" + UID + "&/Duo/" + firstName;
         window.open(url, '_blank');
     }
 }
